0c7e2fde28bfcabc56f5990c5227279a40a41982
todreami <140941478+todreami@users.noreply.github.com>
2024-01-19 09:56:24 +0900

    Add files via upload

---
+++ CodeKata1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeKata1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("homework");
+
+
+
+
+        }
+    }
+}
